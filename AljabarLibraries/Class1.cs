78febbd41c4aa0db6f53a170ba37f9a246dcbe9a
relinggaa <linggayorfel@gmail.com>
2025-05-04 12:57:12 +0700

    done

---
+++ AljabarLibraries/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0], b = persamaan[1], c = persamaan[2];
+            double diskriminan = b * b - 4 * a * c;
+            double akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+            return new double[] { akar1, akar2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            return new double[] { a * a, 2 * a * b, b * b };
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AljabarLibraries;
+
+namespace MainConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] hasil1 = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine("Akar: " + hasil1[0] + ", " + hasil1[1]);
+
+            double[] hasil2 = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine("Hasil Kuadrat: " + hasil2[0] + ", " + hasil2[1] + ", " + hasil2[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
